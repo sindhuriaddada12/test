6e94bb54c36fa7f2bcb1a855d876cf5af6f3e181
sindhuriaddada12 <43210890+sindhuriaddada12@users.noreply.github.com>
2018-09-17 11:47:38 +0100

    index changed

--- src/test/java/ParameterizedUnitTestFib.java
+++ src/test/java/ParameterizedUnitTestFib.java
@@ -13,7 +13,7 @@ public class ParameterizedUnitTestFib {
     public int sInput;
     @Parameterized.Parameter(1)
     public int wExpected;
-    @Parameterized.Parameters(name = "index}: stairs: {0}= ways2Climb :{1}")
+    @Parameterized.Parameters(name = "index}: stairs: {0}= output :{1}")
     public static Iterable<Object[]> data() {
 
 // testing with input values 0,1,2,3,4,5,-3
